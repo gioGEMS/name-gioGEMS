56ee7e6a8cf349ceca79705fe2842801dfacbfba
gio <giogemsllc@gmail.com>
2025-02-19 12:25:44 -0700

    Added laugh.  Got rid of music.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 def on_forever():
-    basic.show_string("gioGEMS")
-    music.play(music.string_playable("C C5 C E C A C G ", 220),
-        music.PlaybackMode.LOOPING_IN_BACKGROUND)
-    music.set_volume(10)
+    music.set_volume(15)
     music.play(music.builtin_playable_sound_effect(soundExpression.giggle),
         music.PlaybackMode.UNTIL_DONE)
+    basic.show_string("gioGEMS")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 basic.forever(function on_forever() {
-    basic.showString("gioGEMS")
-    music.play(music.stringPlayable("C C5 C E C A C G ", 220), music.PlaybackMode.LoopingInBackground)
-    music.setVolume(10)
+    music.setVolume(15)
     music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    basic.showString("gioGEMS")
 })
