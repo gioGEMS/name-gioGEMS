be2c21ef68c26240fbf670744eda22ccd2549612
gio <giogemsllc@gmail.com>
2025-02-19 12:40:56 -0700

    Final name badge.  Eth Denver event.

--- main.py
+++ main.py
@@ -1,4 +1,97 @@
 def on_forever():
+    basic.show_leds("""
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # . .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # #
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . #
+        . # # # #
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . . #
+        # . . . #
+        . # # # #
+        """)
+    basic.show_leds("""
+        . # # # .
+        # . . . .
+        # . . # #
+        # . . . #
+        . # # # #
+        """)
+    basic.pause(100)
+    basic.clear_screen()
     music.set_volume(15)
     music.play(music.builtin_playable_sound_effect(soundExpression.giggle),
         music.PlaybackMode.UNTIL_DONE)
--- main.ts
+++ main.ts
@@ -1,4 +1,97 @@
 basic.forever(function on_forever() {
+    basic.showLeds(`
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . #
+        # . . . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . # #
+        # . . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.clearScreen()
     music.setVolume(15)
     music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     basic.showString("gioGEMS")
